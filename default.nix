5d17de7ba388ca98eb22f63d1ff4f08bdb66be15
Jylhis <markus@jylhis.com>
2024-07-27 14:52:22 +0200

    Move to CMake

--- default.nix
+++ default.nix
@@ -4,13 +4,14 @@
   qmake,
   qt5,
   wrapQtAppsHook,
+  cmake, ninja,
 }:
 stdenv.mkDerivation {
   pname = "lappbill";
   version = "0.1";
 
-  src = ./.;
+  src = lib.sources.trace (lib.cleanSource ./.);
 
   buildInputs = [qt5.full];
-  nativeBuildInputs = [qmake wrapQtAppsHook];
+  nativeBuildInputs = [cmake ninja qmake wrapQtAppsHook qt5.full];
 }
